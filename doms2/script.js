4868ff3f0f57403b5b5418b1ddad5b5713757255
peeyush013 <116724510+peeyush013@users.noreply.github.com>
2022-12-01 12:07:44 +0530

    Add files via upload

---
+++ doms2/script.js
@@ -0,0 +1,24 @@
+document.querySelector('h1').addEventListener('click',function(){
+    document.querySelector('h1').style.color = 'orange'
+})
+
+document.querySelector('h2').addEventListener('click',function(){
+    document.querySelector('h2').style.color = 'yellow'
+})
+
+document.querySelector('h3').addEventListener('click',function(){
+    document.querySelector('h3').style.color = 'blue'
+})
+
+document.querySelector('h4').addEventListener('click',function(){
+    document.querySelector('h4').style.color = 'white'
+})
+
+document.querySelector('h5').addEventListener('click',function(){
+    document.querySelector('h5').style.color = 'skyblue'
+})
+
+document.querySelector('h6').addEventListener('click',function(){
+    document.querySelector('h6').style.color = 'red'
+})
+
