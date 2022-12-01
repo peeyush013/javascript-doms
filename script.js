7f5214dc46dd4f1091912fe23b925f20dd127f35
peeyush013 <116724510+peeyush013@users.noreply.github.com>
2022-12-01 11:45:20 +0530

    dom 1

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 document.querySelector('h1').addEventListener('click',function(){
-    document.querySelector('h1').style.color = 'orange'
+    document.querySelector('h1').style.color = 'red'
+    document.querySelector('h1').style.backgroundColor = 'pink'
+    document.querySelector('h1').textContent='Oneplus Nord'
 })
-
-document.querySelector('h2').addEventListener('click',function(){
-    document.querySelector('h2').style.color = 'yellow'
-})
-
-document.querySelector('h3').addEventListener('click',function(){
-    document.querySelector('h3').style.color = 'blue'
-})
-
-document.querySelector('h4').addEventListener('click',function(){
-    document.querySelector('h4').style.color = 'white'
-})
-
-document.querySelector('h5').addEventListener('click',function(){
-    document.querySelector('h5').style.color = 'skyblue'
-})
-
-document.querySelector('h6').addEventListener('click',function(){
-    document.querySelector('h6').style.color = 'red'
-})
-
