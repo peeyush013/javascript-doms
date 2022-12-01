fdbe8b2c90d1f08e2b88820bfb8bc2c3766c5870
peeyush013 <116724510+peeyush013@users.noreply.github.com>
2022-12-01 12:01:41 +0530

    Add files via upload

---
+++ doms1/script.js
@@ -0,0 +1,5 @@
+document.querySelector('h1').addEventListener('click',function(){
+    document.querySelector('h1').style.color = 'red'
+    document.querySelector('h1').style.backgroundColor = 'pink'
+    document.querySelector('h1').textContent='Oneplus Nord'
+})
